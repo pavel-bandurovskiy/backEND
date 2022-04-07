9a591f9e246b5e477e68fa707502b586c6594e7f
BogdanMikhaenko <99245043+BogdanMikhaenko@users.noreply.github.com>
2022-04-07 10:51:08 +0300

    Update index.php

--- 5/index.php
+++ 5/index.php
@@ -117,8 +117,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
       print('Error : ' . $e->getMessage());
       exit();
     }
-    //printf('Вход с логином %s, uid %d', $_SESSION['login'], $_SESSION['uid']);
-    echo "<div>Вход с логином $_SESSION['login'], uid $_SESSION['uid']</div>";
+    printf('<div>Вход с логином %s, uid %d</div>', $_SESSION['login'], $_SESSION['uid']);
   }
   include('form.php');
 }
